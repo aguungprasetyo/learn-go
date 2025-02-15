2011f098267ddf48a923baab5040d1a27842c71e
Agung Prasetyo <agung.nugroho25@gmail.com>
2025-02-15 12:10:06 +0700

    try variable

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+// declare outsite function
+var a int
+var b float32
+var c string
+var d bool
+
 func main() {
-	fmt.Println("Hello, Ozone Digitech")
+
+	a = 1
+	b = 3.16
+	c = "Hello, Ozone Digitech"
+	d = true
+	e := 1 //this sign only can use inside the function
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
 }
